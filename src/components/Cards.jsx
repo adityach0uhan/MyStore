103522bc22181d96aeca49f799ec79491a748da1
Visinigiri Aditya <adityakumarverified@gmail.com>
2023-07-26 20:15:29 +0530

    Changes

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,27 +1,27 @@
-import React from 'react'
-import './styles/Cards.css'
-
+import React from "react";
+import "./styles/Cards.css";
 
 const Cards = ({ productData }) => {
-    // {
-    //     productData.map((currentElement) => {
-    //         return currentElement.index;
-    //     })
-    // }
-    return (
-        
-        <>
-          
-            <div className='Main-Card-Container' >
-                <div className='Card-image'> <img src="" alt="IMG" /></div>
-                <h5>hbhjbj</h5>
-                <h5>Title</h5>
-                <p>discription</p>
-                <p>price</p>
-                <p>rating</p>
-            </div >
-        </>
-    )
-}
+  console.log(productData);
+  return (
+    <>
+      <div className="Main-Card-Container">
+        {productData.products.map((product) => (
+          <div className="Card-Container">
+            <div className="Card-image">
+              {" "}
+              <img src={product.thumbnail} alt="IMG" />
+            </div>
+            <h5>{product.brand}</h5>
+            <h5>{product.title}</h5>
+            <p>{product.description}</p>
+            <p>{product.price}</p>
+            <p>{product.rating}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
 
-export default Cards
+export default Cards;
